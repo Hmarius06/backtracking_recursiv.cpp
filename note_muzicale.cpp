c3008c4dabfe4d0f0680f2b04d5e8a2a936b9f9d
Hmarius06 <158442908+Hmarius06@users.noreply.github.com>
2024-10-17 10:30:04 +0300

    Create note_muzicale.cpp

---
+++ note_muzicale.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+using namespace std;
+int st[100],n,nr,a[10];
+char note[10][10]={" ","do","re","mi","fa","so","la","si"};
+
+void afisare(int k)
+{
+    cout<<" Solutia "<<nr<<": ";
+    cout<<note[st[1]];
+    for(int i=2;i<k;i++)
+       cout<<","<<note[st[i]];
+    cout<<endl;
+}
+
+void backtrack(int k)
+{
+    int i;
+    if(k==n+1){nr++;afisare(k);}
+    else
+        {
+            st[k]=0;
+            while(st[k]<=k)
+            {
+                st[k]++;
+                backtrack(k+1);
+            }
+        }
+
+}
+
+int main()
+{
+
+    cout<<"n=";cin>>n;
+    if(n<6)backtrack(1);
+    cout<<nr<< " solutii";
+
+return 0;
+}
